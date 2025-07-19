679c8df9131053a07ab022489a343b8ebfdb83af
nlhtungg <nlhtungg@gmail.com>
2025-07-19 16:24:20 +0000

    remove scripts

--- scripts/kafka_stuff.sh
+++
@@ -1,9 +0,0 @@
-docker exec -it broker /bin/bash
-
-kafka-console-consumer --topic financial_transactions --bootstrap-server localhost:9092 --from-beginning &
-
-#delete topic
-docker exec -it e489366b6157 /usr/bin/kafka-topics --bootstrap-server localhost:9092 --delete --topic financial_transactions
-
-#create topic
-docker exec -it e489366b6157 /usr/bin/kafka-topics --bootstrap-server localhost:9092 --create --topic financial_transactions --partitions 1 --replication-factor 1
--- scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#docker
-docker compose up -d &
-
-#start flink cluster
-/opt/flink/bin/start-cluster.sh &
-
-#submit job to flink cluster
-/opt/flink/bin/flink run -c flinkcommerce.DataStreamJob target/flinkcommerce-1.0-SNAPSHOT.jar
